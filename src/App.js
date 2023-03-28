137f25ab1a0ee3dd8106034a5976cf29ec7fa51c
filipeMarques00 <113559744+filipeMarques00@users.noreply.github.com>
2023-03-28 14:26:05 -0300

    Adicionando funcionalidade de scroll

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Tmdb from "./Tmdb";
 import MovieRow from "./components/MovieRow";
 import "./App.css";
 import FeaturedMovie from "./components/FeaturedMovie";
+import Header from "./components/Header";
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(()=> {
     const loadAll = async () => {
@@ -29,8 +31,28 @@ export default () => {
   },
   []);
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+
+      window.addEventListener('scroll', scrollListener);
+    }
+  }, []);
+
   return(
     <div className="page">
+
+      <Header black = {blackHeader}/>
+      
       {
         featuredData&&
         <FeaturedMovie item={featuredData}/>
@@ -41,7 +63,11 @@ export default () => {
             <MovieRow key={key} title = {item.title} items = {item.items}/>
           ))}
       </section>
-
+      
+      <footer>
+        Direitos de imagem reservados a Netflix<br />
+        Dados pegos do site Themoviedb.org
+      </footer>
     </div>
   );
 }
